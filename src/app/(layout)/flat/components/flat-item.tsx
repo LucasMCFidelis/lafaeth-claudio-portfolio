79caf3f1bfad00a9a86bc93e96a96019d9b626cd
Lucas Fidelis <lucasm241301@gmail.com>
2025-09-27 22:25:22 -0300

    refactor: add support children in FlatItem for server-side rendering of backImage and labels

--- src/app/(layout)/flat/components/flat-item.tsx
+++ src/app/(layout)/flat/components/flat-item.tsx
@@ -2,15 +2,16 @@
 
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { memo, useRef, useState } from "react";
 
 import { FlatDTO } from "@/app/data/flat/flat-dto";
 
 interface FlatItemProps {
   flat: FlatDTO;
+  children: React.ReactNode;
 }
 
-const FlatItem = ({ flat }: FlatItemProps) => {
+const FlatItem = memo(({ flat, children }: FlatItemProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [dividerX, setDividerX] = useState(50);
   const [dragging, setDragging] = useState(false);
@@ -43,15 +44,6 @@ const FlatItem = ({ flat }: FlatItemProps) => {
             "h-[250px] w-[177px] sm:h-[350px] sm:w-[248px] md:h-[450px] md:w-[318px] lg:h-[600px] lg:w-[424px] xl:h-[750px] xl:w-[530px]"
       }`}
     >
-      {flat.backImage?.imageUrl && (
-        <Image
-          src={flat.backImage?.imageUrl}
-          alt={`${flat.title} - flat`}
-          fill
-          className={`object-contain overflow-hidden object-left z-10`}
-        />
-      )}
-
       <div
         className="absolute top-0 bottom-0 z-30 w-px bg-primary select-none"
         style={{ left: `${dividerX}%`, transform: "translateX(-50%)" }}
@@ -65,28 +57,25 @@ const FlatItem = ({ flat }: FlatItemProps) => {
         </div>
       </div>
 
+      {children}
+
       {flat.frontImage?.imageUrl && (
         <div
           className="relative z-20 overflow-hidden"
           style={{ width: `${dividerX}%` }}
         >
           <Image
-            src={flat.frontImage.imageUrl}
+            src={flat.frontImage?.imageUrl}
             alt={`${flat.title} - line`}
             fill
             className={`object-cover object-left`}
           />
         </div>
       )}
-
-      <div className="absolute z-30 w-full flex justify-between text-primary-foreground text-[0.5rem]">
-        {flat.screenwriter && (
-          <p className="bg-primary/70 px-2">Roteiro: {flat.screenwriter}</p>
-        )}
-        <p className="bg-primary/70 px-2">Arte: {flat.artist}</p>
-      </div>
     </div>
   );
-};
+});
+
+FlatItem.displayName = "FlatItem";
 
 export default FlatItem;
--- src/app/(layout)/flat/page.tsx
+++ src/app/(layout)/flat/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 import getManyFlats from "@/app/data/flat/get-many-flats";
 
 import FlatItem from "./components/flat-item";
@@ -12,7 +14,25 @@ const FlatPage = async () => {
     <div className="px-5 flex-1 flex flex-col">
       <div className="flex-1 flex flex-col gap-5 sm:gap-10 items-center">
         {flats.map((flat) => (
-          <FlatItem key={flat.id} flat={flat} />
+          <FlatItem key={flat.id} flat={flat}>
+            {flat.backImage?.imageUrl && (
+              <Image
+                src={flat.backImage?.imageUrl}
+                alt={`${flat.title} - flat`}
+                fill
+                className={`object-contain overflow-hidden object-left z-10`}
+              />
+            )}
+
+            <div className="absolute z-30 w-full flex justify-between text-primary-foreground text-[0.5rem]">
+              {flat.screenwriter && (
+                <p className="bg-primary/70 px-2">
+                  Roteiro: {flat.screenwriter}
+                </p>
+              )}
+              <p className="bg-primary/70 px-2">Arte: {flat.artist}</p>
+            </div>
+          </FlatItem>
         ))}
       </div>
     </div>
